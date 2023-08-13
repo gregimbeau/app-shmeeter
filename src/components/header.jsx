8aa90e462000666d012811552b799cbcf7e0db95
Guillaume Regimbeau <gregimbeau@mail.com>
2023-08-13 16:58:16 +0200

    header update

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -2,43 +2,44 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 import Cookies from "js-cookie";
 
-
 const Header = () => {
   const navigate = useNavigate();
-
   const jwt = localStorage.getItem("jwt");
 
-const handleLogout = () => {
-  localStorage.removeItem("jwt");
-  Cookies.remove("token");
-  navigate("/"); // Redirect to login after logout
-};
-return (
-  <div className='header'>
-    <div className='container'>
-      <div className='nav'>
-        <button className='nav-item' onClick={() => navigate("/")}>
-          Home
-        </button>
-        <button className='nav-item' onClick={() => navigate("/form")}>
-          S'inscrire
-        </button>
-        {!jwt ? (
-          <button className='nav-item' onClick={() => navigate("/login")}>
-            Login
+  const handleLogout = () => {
+    localStorage.removeItem("jwt");
+    Cookies.remove("token");
+    navigate("/"); // Redirect to login after logout
+  };
+
+  return (
+    <div className='header'>
+      <div className='container'>
+        <div className='nav'>
+          <button className='nav-item' onClick={() => navigate("/")}>
+            Home
           </button>
-        ) : (
-          <button className='nav-item' onClick={handleLogout}>
-            Logout
+          {!jwt && (
+            <button className='nav-item' onClick={() => navigate("/form")}>
+              S'inscrire
+            </button>
+          )}
+          {!jwt ? (
+            <button className='nav-item' onClick={() => navigate("/login")}>
+              Login
+            </button>
+          ) : (
+            <button className='nav-item' onClick={handleLogout}>
+              Logout
+            </button>
+          )}
+          <button className='nav-item' onClick={() => navigate("/profile")}>
+            Profile
           </button>
-        )}
-        <button className='nav-item' onClick={() => navigate("/profile")}>
-          Profile
-        </button>
+        </div>
       </div>
     </div>
-  </div>
-);
+  );
 };
 
 export default Header;
