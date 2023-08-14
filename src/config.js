385230b0625d0dda2a3d48f0751fa484bd1eca74
Guillaume Regimbeau <gregimbeau@mail.com>
2023-08-14 19:23:10 +0200

    likes

--- src/config.js
+++ src/config.js
@@ -1,9 +1,9 @@
 let API_BASE_URL;
 
 // if (process.env.NODE_ENV === "production") {
-  // API_BASE_URL = "https://app-shmeeter-server-production.up.railway.app";
+  API_BASE_URL = "https://app-shmeeter-server-production.up.railway.app";
 // } else {
-  API_BASE_URL = "http://localhost:1337";
+  // API_BASE_URL = "http://localhost:1337";
 // }
 
 export { API_BASE_URL };
