35efd4acfd365a45c30ded307820288ef27a48a1
Guillaume Regimbeau <gregimbeau@mail.com>
2023-08-14 09:13:39 +0200

    avatar pic on

--- src/components/profile.jsx
+++ src/components/profile.jsx
@@ -1,7 +1,9 @@
 import React, { useState, useEffect, useContext } from "react";
 import Cookies from "js-cookie";
 import { GlobalStateContext } from "../GlobalStateContext";
-import { Image } from "cloudinary-react";
+import { AdvancedImage } from "@cloudinary/react";
+import { Cloudinary } from "@cloudinary/url-gen";
+import { fill } from "@cloudinary/url-gen/actions/resize";
 
 const Profile = () => {
   const { state, dispatch } = useContext(GlobalStateContext);
@@ -18,72 +20,41 @@ const Profile = () => {
     email: "",
     password: "",
     description: "",
+    avatarUrl: "",
   });
 
-  const uploadWidget = () => {
-    window.cloudinary.openUploadWidget(
-      {
-        cloudName: "dmq3cpw6u",
-        uploadPreset: "eom5grdo",
-        sources: ["local", "url", "camera", "facebook", "dropbox"],
-        defaultSource: "local",
-      },
-      (error, result) => {
-        if (!error && result && result.event === "success") {
-          const uploadedAvatarUrl = result.info.url;
-          setAvatarUrl(uploadedAvatarUrl);
+  const cld = new Cloudinary({
+    cloud: {
+      cloudName: "dmq3cpw6u",
+    },
+  });
 
-          // Remove or comment this line
-          // storeAvatarOnServer(uploadedAvatarUrl);
-        } else if (error) {
-          console.error("Cloudinary upload error:", error);
-        }
+  const transformedAvatar = avatarUrl
+    ? cld
+        .image(avatarUrl.split("/").pop().split(".")[0])
+        .resize(fill().width(250).height(250))
+    : null;
+
+const uploadWidget = () => {
+  window.cloudinary.openUploadWidget(
+    {
+      cloudName: "dmq3cpw6u",
+      uploadPreset: "eom5grdo",
+      sources: ["local", "url", "camera", "facebook", "dropbox"],
+      defaultSource: "local",
+    },
+    async (error, result) => {
+      if (!error && result && result.event === "success") {
+        const uploadedAvatarUrl = result.info.url;
+        setAvatarUrl(uploadedAvatarUrl);
+
+        setFormData((prev) => ({ ...prev, avatarUrl: uploadedAvatarUrl }));
       }
-    );
-  };
-
-  const storeAvatarOnServer = async (url) => {
-    if (!userProfile || !userProfile.id) {
-      console.error("No user profile loaded.");
-      return;
     }
+  );
+};
 
-    try {
-      const token = Cookies.get("token");
-      const response = await fetch(
-        `https://app-shmeeter-server-production.up.railway.app/api/users/${userProfile.id}/avatar`,
-        {
-          method: "PUT",
-          headers: {
-            Authorization: `Bearer ${token}`,
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({ avatarUrl: url }),
-        }
-      );
 
-      if (response.status === 405) {
-        console.error("PUT method not allowed at this endpoint.");
-      } else if (!response.ok) {
-        console.error("Error with request:", response.statusText);
-      }
-
-      if (response.headers.get("content-type").includes("application/json")) {
-        const data = await response.json();
-        if (data.success) {
-          console.log("Avatar stored successfully on the server!");
-        } else {
-          console.error("Error storing avatar on server:", data.message);
-        }
-      } else {
-        // Handle non-JSON response (e.g., plain text or HTML)
-        const text = await response.text();
-        console.error("Error from server:", text);
-      }
-    } catch (error) {
-      console.error("Error storing avatar:", error);
-    }
-  };
 
   useEffect(() => {
     const fetchUserProfile = async () => {
@@ -103,15 +74,14 @@ const Profile = () => {
           const data = await response.json();
           setUserProfile(data);
           dispatch({ type: "SET_USER", payload: data }); // Update the global state with fetched user data
-          setAvatarUrl(data.avatarUrl || null); // Set the avatarUrl from the fetched data
 
           setFormData({
             username: data.username || "",
             displayName: data.displayName || "",
             email: data.email || "",
             description: data.description || "",
+            avatarUrl: data.avatarUrl || "",
           });
-          setAvatarUrl(data.avatarUrl || null); // Set the avatarUrl from the fetched data
         } catch (error) {
           console.error("Error fetching profile:", error);
         }
@@ -126,48 +96,41 @@ const Profile = () => {
     setFormData((prev) => ({ ...prev, [name]: value }));
   };
 
-  const handleProfileUpdate = async (e) => {
-    e.preventDefault();
+const handleProfileUpdate = async (e, updatedData = formData) => {
+  if (e && e.preventDefault) {
+    e.preventDefault(); // Check if e is an event to prevent default
+  }
 
-    try {
-      const token = Cookies.get("token");
-      const response = await fetch(
-        `https://app-shmeeter-server-production.up.railway.app/api/users/${userProfile.id}`,
-        {
-          method: "PUT",
-          headers: {
-            Authorization: `Bearer ${token}`,
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify(formData),
-        }
-      );
+  try {
+    const token = Cookies.get("token");
 
-      const data = await response.json();
-      setUserProfile(data);
-      dispatch({ type: "SET_USER", payload: data });
+    console.log("Sending data:", updatedData); // <-- Add this log for sent data
 
-      setFormData({
-        username: data.username || "",
-        displayName: data.displayName || "",
-        email: data.email || "",
-        description: data.description || "",
-      });
-
-      setAvatarUrl(data.avatarUrl || null); // Set the avatarUrl from the fetched data
-
-      if (data.id) {
-        console.log("Profile updated!");
-
-        setUserProfile((prev) => ({ ...prev, ...formData }));
-        dispatch({ type: "SET_USER", payload: formData }); // Update the global state after the profile is updated
-      } else {
-        console.error("Error updating profile:", data.message);
+    const response = await fetch(
+      `https://app-shmeeter-server-production.up.railway.app/api/users/${userProfile.id}`,
+      {
+        method: "PUT",
+        headers: {
+          Authorization: `Bearer ${token}`,
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(updatedData),
       }
-    } catch (error) {
-      console.error("There was an error updating the profile:", error);
-    }
-  };
+    );
+
+    const data = await response.json();
+
+    console.log("Received data:", data); // <-- Add this log for received data
+
+    setUserProfile(data);
+    dispatch({ type: "SET_USER", payload: data });
+
+    // ... (rest of the function)
+  } catch (error) {
+    console.error("There was an error updating the profile:", error);
+  }
+};
+
 
   if (!userProfile) return <p>You must log in first...</p>;
 
@@ -178,21 +141,22 @@ const Profile = () => {
           <div className='profile-container'>
             {avatarUrl && (
               <div className='avatar-container'>
-                <Image
-                  cloudName='dmq3cpw6u'
-                  publicId={avatarUrl}
-                  width='200'
-                  height='200'
-                  crop='thumb'
-                />
+                {transformedAvatar && (
+                  <div className='avatar-container'>
+                    <AdvancedImage cldImg={transformedAvatar} />
+                  </div>
+                )}
               </div>
             )}
 
             <div className='profile-header'>
               <h1>Welcome, {userProfile.displayName}</h1>
             </div>
+            <td>
+              <img src={avatarUrl || userProfile.avatarUrl} alt='User Avatar' />
+            </td>
             <button className='btn btn--upload' onClick={uploadWidget}>
-              Upload Avatar
+              Upload Avatar or change it
             </button>
             <div className='profile-content'>
               <h2>This is your profile</h2>
@@ -212,15 +176,20 @@ const Profile = () => {
                     <td>{userProfile.email}</td>
                   </tr>
                   <tr>
-                    <td>Role:</td>
+                    <td>Description:</td>
                     <td>{userProfile.description}</td>
+                  </tr>
+                  <tr>
+                    <td>AvatarUrl:</td>
+                    <td>{avatarUrl || userProfile.avatarUrl}</td>
                   </tr>
                 </tbody>
               </table>
 
               <h2>Edit Profile</h2>
 
-              <form onSubmit={handleProfileUpdate}>
+              <form onSubmit={(e) => handleProfileUpdate(e)}>
+                {" "}
                 <div className='form-group'>
                   <label>Displayed Name:</label>
                   <input
@@ -231,7 +200,16 @@ const Profile = () => {
                     required
                   />
                 </div>
-
+                <div className='form-group'>
+                  <label>avatarUrl:</label>
+                  <input
+                    type='text'
+                    name='avatarUrl'
+                    value={formData.avatarUrl}
+                    onChange={handleChange}
+                    required
+                  />
+                </div>
                 <div className='form-group'>
                   <label>Description:</label>
                   <textarea
@@ -242,7 +220,6 @@ const Profile = () => {
                     required
                   />
                 </div>
-
                 <button className='btn btn--primary' type='submit'>
                   Update Profile
                 </button>
